3557a2066fa50828e568bbb414f7c7da46974d28
Frixu <frixu@frixu.me>
2017-10-08 20:38:53 +0200

    Trying to add coverage reports

--- src/test/java/com/koem/bedwars/player/ExampleTest.java
+++ src/test/java/com/koem/bedwars/player/ExampleTest.java
@@ -3,6 +3,8 @@ package com.koem.bedwars.player;
 import static junit.framework.Assert.*;
 import static org.mockito.Mockito.*;
 
+import com.koem.bedwars.BedWars;
+import com.koem.bedwars.player.TeamManager.TEAM;
 import org.bukkit.entity.Player;
 import org.junit.jupiter.api.*;
 
@@ -10,12 +12,30 @@ class ExampleTest {
 	
 	@Test
 	@DisplayName("An example on how to use JUnit testing framework.")
-	void doubleJoinTest() {
-		PlayerManager manager = new PlayerManager(null);
-		Player player = mock(Player.class);
-		manager.createPlayer(player);
-		manager.createPlayer(player);
-		manager.removePlayer(player);
-		assertTrue(manager.getBWPlayers().isEmpty());
+	void duplicateTest() {
+	    PlayerManager manager = new PlayerManager(null);
+	    Player player = mock(Player.class);
+	    manager.createPlayer(player);
+	    assertFalse(manager.getBWPlayers().isEmpty());
+	    manager.createPlayer(player);
+	    manager.removePlayer(player);
+	    assertTrue(manager.getBWPlayers().isEmpty());
 	}
+
+	/* Zajme sie tym pozniej
+	@Test
+	@DisplayName("This test is supposed to fail.")
+	void multipleTeamsTest() {
+	    BedWars plugin = mock(BedWars.class);
+	    plugin.onEnable();
+	    Player player = mock(Player.class);
+	    plugin.getPlayerManager().createPlayer(player);
+	    plugin.getPlayerManager().getBWPlayer(player).setTeam(TEAM.RED);
+	    assertTrue(plugin.getTeamManager().getTeam(TEAM.RED).getTeamPlayers() == 1);
+	    assertTrue(plugin.getTeamManager().getTeam(TEAM.GREEN).getTeamPlayers() == 0);
+	    plugin.getPlayerManager().getBWPlayer(player).setTeam(TEAM.GREEN);
+		assertTrue(plugin.getTeamManager().getTeam(TEAM.RED).getTeamPlayers() == 0);
+		assertTrue(plugin.getTeamManager().getTeam(TEAM.GREEN).getTeamPlayers() == 1);		
+	}
+	*/
 }
